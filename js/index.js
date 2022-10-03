5d79cc9f87d5a42ab7782ec985f3cf9fe4149de8
Natalia Matat <n.matat@gmail.com>
2022-10-03 21:28:32 +0300

    add rendering images

--- js/index.js
+++ js/index.js
@@ -1,12 +1,68 @@
 import { catsData } from './data.js';
 
 const emotionRadios = document.querySelector('#emotion-radios');
+const getImageBtn = document.getElementById('get-image-btn');
+const gifsOnlyOption = document.getElementById('gifs-only-option');
+const memeModalInner = document.getElementById('meme-modal-inner');
+const memeModal = document.getElementById('meme-modal');
+const memeModalCloseBtn = document.getElementById('meme-modal-close-btn');
+
+memeModalCloseBtn.addEventListener('click', closeModal);
 
 emotionRadios.addEventListener('change', highlightCheckedOption);
 
+getImageBtn.addEventListener('click', renderCat);
+
 function highlightCheckedOption(event) {
+	const radios = document.getElementsByClassName('radio');
+
+	for (let radio of radios) {
+		radio.classList.remove('highlight');
+	}
+
 	document.getElementById(event.target.id).parentElement.classList.add('highlight');
-	console.log(event.target.id);
+}
+
+function closeModal() {
+	memeModal.style.display = 'none';
+}
+
+function renderCat() {
+	const catObject = getSingleCatObject();
+	memeModalInner.innerHTML = `
+<img 
+        class="cat-img" 
+        src="./images/${catObject.image}"
+        alt="${catObject.alt}"
+        >
+`;
+	memeModal.style.display = 'flex';
+}
+
+function getSingleCatObject() {
+	const catsArray = getMatchingCatsArray();
+	if (catsArray.length === 1) {
+		return catsArray[0];
+	} else {
+		const randomNumber = Math.floor(Math.random() * catsArray.length);
+		return catsArray[randomNumber];
+	}
+}
+
+function getMatchingCatsArray() {
+	if (document.querySelector('input[type="radio"]:checked')) {
+		const selectedEmotion = document.querySelector('input[type="radio"]:checked').value;
+		const isGif = gifsOnlyOption.checked;
+
+		const matchingCatsArray = catsData.filter(function (cat) {
+			if (isGif) {
+				return cat.emotionTags.includes(selectedEmotion) && cat.isGif;
+			} else {
+				return cat.emotionTags.includes(selectedEmotion);
+			}
+		});
+		return matchingCatsArray;
+	}
 }
 
 function getEmotionsArray(cats) {
@@ -36,9 +92,3 @@ function renderEmotionsRadios(cats) {
 }
 
 renderEmotionsRadios(catsData);
-
-/*
- document.getElementById(e.target.id).parentElement.style.backgroundColor
-  document.getElementById(e.target.id).parentElement.classList.add('read')
-    document.getElementById(e.target.id).parentElement.classList.remove('unread')
- */
